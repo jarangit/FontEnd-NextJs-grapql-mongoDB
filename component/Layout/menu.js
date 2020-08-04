549d3feae4bdbc8d69977a5b587ae6066587989e
jarangit <jaran.dch@gmail.com>
2020-08-04 20:46:11 +0700

    sty menu

--- component/Layout/menu.js
+++ component/Layout/menu.js
@@ -1,15 +1,64 @@
 import Link from 'next/link'
 import React, { useContext } from 'react'
 import { AuthContext } from '../../appState/authProvider'
+import styled from 'styled-components'
 
+const ButLogout = styled.button`
+    background: none;
+    color: #ff00aa;
+    border: 1px solid #ff00aa;
+    padding: 10px;
+    border-radius: 0.7rem;
+    :hover{
+        background: #ff00aa;
+        color: white;
+    }
+`
 
+const MenuSyt = styled.div`
+    background: black;
+    color: white;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    height: 100px;
+    align-items: center;
+
+    a {
+        color: white;
+        text-decoration: none;
+        :hover{
+            color: #ff00aa;
+        }
+    }
+    ul{
+        display: flex;
+        align-items: center;
+        li{
+            list-style: none;
+            margin: 0 15px;
+        }
+            .email-user{
+                a{
+                    color: #ff00aa;
+                    :hover{
+                        text-decoration: underline;
+                        text-decoration-color: #ff00aa;
+                        transition: 0.3s;
+
+                    }
+                }
+            }
+    }
+
+`
 const Menu = () => {
 
     const { user, Signout } = useContext(AuthContext)
 
     console.log(user)
     return(
-        <div>
+        <MenuSyt>
             <h1> THE GUITAR NEXT </h1>
             <ul>
                 <li>
@@ -29,10 +78,12 @@ const Menu = () => {
                             <a> Cart </a>
                         </Link>
                     </li>
-                    <button onClick = { Signout } > Logout </button>
-                    <li>
-                        <strong> {user.name} </strong>
+                    <li className = "email-user">
+                        <Link href = "/cart" >
+                            <a> {user.email} </a>
+                        </Link>
                     </li>
+                    <ButLogout onClick = { Signout } > Logout </ButLogout>
                    </>
                 )}
                 {!user && (
@@ -50,7 +101,7 @@ const Menu = () => {
                    </>
                 )}
             </ul>
-        </div>
+        </MenuSyt>
     )
 }
 export default Menu
